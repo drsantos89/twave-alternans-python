2f70b08d7aa1bf6acc3ed6cc8dd9bda7b0248219
Diogo Santos <santos@transformative.ai>
2023-02-26 16:23:09 +0100

    fix beat correlation

--- src/twaextractor/main.py
+++ src/twaextractor/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import pydantic
 import wfdb
 from scipy.signal import butter, sosfiltfilt
+from wfdb import processing
 
 
 class TWAExtractor(pydantic.BaseModel):
@@ -42,6 +43,8 @@ class TWAExtractor(pydantic.BaseModel):
         self._build_beat_matrix()
         logging.info("correcting baseline ...")
         self._correct_baseline()
+        logging.info("calculating beat medians ...")
+        self._calc_beat_medians()
         logging.info("replacing low correlation beats ...")
         self._replace_low_correlation_beats()
         logging.info(
@@ -70,18 +73,59 @@ class TWAExtractor(pydantic.BaseModel):
 
     def _detect_beats(self) -> None:
         """Detect beats in ECG signal."""
-        self.qrs = wfdb.ecg.gqrs_detect(
-            signal=self.signal_filt[:, 0], sampling_rate=self.fs
-        )["qrs"]
+        self.qrs = processing.gqrs_detect(d_sig=self.signal_filt[:, 0], fs=self.fs)
 
     def _build_beat_matrix(self) -> None:
         """Build beat matrix from ECG signal."""
-        beat_matrix = np.zeros((len(self.qrs), 2 * self.fs))
+        if self.fs is None:
+            raise ValueError("fs must be defined")
+        self.beat_matrix = np.zeros((len(self.qrs), int(0.5 * self.fs)))
         for i, beat in enumerate(self.qrs):
-            beat_matrix[i, :] = self.signal_filt[beat - self.fs : beat + self.fs, 0]
+            if beat - int(0.1 * self.fs) < 0:
+                continue
+            if beat + int(0.4 * self.fs) > len(self.signal_filt):
+                continue
+            self.beat_matrix[i, :] = self.signal_filt[
+                beat - int(0.1 * self.fs) : beat + int(0.4 * self.fs), 0
+            ]
 
     def _correct_baseline(self) -> None:
-        """Correct baseline in ECG signal."""
-        self.signal_corrected = wfdb.ecg.correct_baseline_wander(
-            signal=self.signal_filt, sampling_rate=self.fs, method="polynomial", order=3
-        )
+        """Correct baseline in beat matrix."""
+        baselines = np.mean(self.beat_matrix, axis=1)
+        self.beat_matrix_corrected = self.beat_matrix - baselines
+
+    def _calc_beat_medians(self) -> None:
+        """Calculate beat medians from beat matrix."""
+        self.beat_median = np.median(self.beat_matrix_corrected, axis=0)
+        self.beat_median_odd = self.beat_median[1::2]
+        self.beat_median_even = self.beat_median[0::2]
+
+    def _replace_low_correlation_beats(self) -> None:
+        """Replace low correlation beats in ECG signal."""
+        self.b = np.zeros(len(self.qrs))
+        for i, beat in self.beat_matrix_corrected[1::2]:
+            if np.corrcoef(beat, self.beat_median_odd)[0, 1] < 0.9:
+                self.b[i] = 1
+                self.beat_matrix_corrected[i, :] = self.beat_median_odd
+        for i, beat in self.beat_matrix_corrected[0::2]:
+            if np.corrcoef(beat, self.beat_median_even)[0, 1] < 0.9:
+                self.b[i] = 1
+                self.beat_matrix_corrected[i, :] = self.beat_median_even
+
+    def _build_t_wave_vector(self) -> None:
+        """Build T-wave vector from ECG signal."""
+        if self.fs is None:
+            raise ValueError("fs must be defined")
+        self.t_wave_vector = np.zeros((len(self.qrs), self.num_signals))
+        for i, beat in enumerate(self.beat_matrix_corrected):
+            self.t_wave_vector[i, :] = beat[int(0.2 * self.fs) : int(0.4 * self.fs)]
+
+    def _calculate_k_score(self) -> None:
+        """Calculate K-score (spectral method) from ECG signal."""
+        if self.num_signals is None:
+            raise ValueError("num_signals must be defined")
+        self.k_score = np.zeros(self.num_signals)
+        for i in range(self.num_signals):
+            self.k_score[i] = np.std(self.t_wave_vector[:, i]) / np.mean(
+                self.t_wave_vector[:, i]
+            )
